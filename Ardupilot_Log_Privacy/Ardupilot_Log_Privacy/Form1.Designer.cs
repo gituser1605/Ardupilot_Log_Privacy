2a55ae298a7be0621069beef01b166c767f98204
gituser1605 <34548904+gituser1605@users.noreply.github.com>
2017-12-15 12:05:59 +0100

    Add files via upload

---
+++ Ardupilot_Log_Privacy/Ardupilot_Log_Privacy/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Ardupilot_Log_Privacy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.ofdLog = new System.Windows.Forms.OpenFileDialog();
+            this.tbxOffset = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblOffset = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.tbxLog = new System.Windows.Forms.TextBox();
+            this.cbxLog = new System.Windows.Forms.CheckBox();
+            this.btnCreateParam = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "open log";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // ofdLog
+            // 
+            this.ofdLog.FileName = "openFileDialog1";
+            // 
+            // tbxOffset
+            // 
+            this.tbxOffset.Location = new System.Drawing.Point(53, 41);
+            this.tbxOffset.Name = "tbxOffset";
+            this.tbxOffset.Size = new System.Drawing.Size(34, 20);
+            this.tbxOffset.TabIndex = 2;
+            this.tbxOffset.TextChanged += new System.EventHandler(this.tbxOffset_TextChanged);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 67);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblOffset
+            // 
+            this.lblOffset.AutoSize = true;
+            this.lblOffset.Location = new System.Drawing.Point(9, 44);
+            this.lblOffset.Name = "lblOffset";
+            this.lblOffset.Size = new System.Drawing.Size(38, 13);
+            this.lblOffset.TabIndex = 4;
+            this.lblOffset.Text = "Offset:";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(93, 17);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(0, 13);
+            this.lblPath.TabIndex = 5;
+            // 
+            // tbxLog
+            // 
+            this.tbxLog.Location = new System.Drawing.Point(12, 96);
+            this.tbxLog.Multiline = true;
+            this.tbxLog.Name = "tbxLog";
+            this.tbxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxLog.Size = new System.Drawing.Size(896, 566);
+            this.tbxLog.TabIndex = 6;
+            // 
+            // cbxLog
+            // 
+            this.cbxLog.AutoSize = true;
+            this.cbxLog.Location = new System.Drawing.Point(96, 71);
+            this.cbxLog.Name = "cbxLog";
+            this.cbxLog.Size = new System.Drawing.Size(76, 17);
+            this.cbxLog.TabIndex = 8;
+            this.cbxLog.Text = "changelog";
+            this.cbxLog.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateParam
+            // 
+            this.btnCreateParam.Location = new System.Drawing.Point(178, 67);
+            this.btnCreateParam.Name = "btnCreateParam";
+            this.btnCreateParam.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateParam.TabIndex = 9;
+            this.btnCreateParam.Text = ".param";
+            this.btnCreateParam.UseVisualStyleBackColor = true;
+            this.btnCreateParam.Click += new System.EventHandler(this.btnCreateParam_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 674);
+            this.Controls.Add(this.btnCreateParam);
+            this.Controls.Add(this.cbxLog);
+            this.Controls.Add(this.tbxLog);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblOffset);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.tbxOffset);
+            this.Controls.Add(this.btnOpenFile);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Ardupilot Logfile Privacy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog ofdLog;
+        private System.Windows.Forms.TextBox tbxOffset;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblOffset;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox tbxLog;
+        private System.Windows.Forms.CheckBox cbxLog;
+        private System.Windows.Forms.Button btnCreateParam;
+    }
+}
+
---
+++ Ardupilot_Log_Privacy/Ardupilot_Log_Privacy/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ardupilot_Log_Privacy
+{
+    public partial class frmMain : Form
+    {
+        string pathR = string.Empty;
+        string pathW = string.Empty;
+        
+        double offset;
+        StreamWriter sw;
+        StreamReader sr;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            tbxOffset.Text = "1";
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            ofdLog.Filter = "log (*.log) |*.log";
+            if (ofdLog.ShowDialog() == DialogResult.OK)
+            {
+                pathR = ofdLog.FileName;
+                lblPath.Text = pathR;
+                
+                try
+                {
+                    sr = new StreamReader(new FileStream(pathR, FileMode.Open));
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message + "\n Cannot open file.");
+                    sr.Close();
+                    return;
+                }
+            }
+        }
+        
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string[] p = pathR.Split('.');
+            pathW = p[0] + "_Private.log";
+            sw = new StreamWriter(pathW, true);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.StartsWith("GPS"))
+                {
+                    sw.WriteLine(changePos(line, 7, offset));
+                }
+                else if (line.StartsWith("POS"))
+                {
+                    sw.WriteLine(changePos(line, 2, offset));
+                }
+                else if (line.StartsWith("AHR2"))
+                {
+                    sw.WriteLine(changePos(line, 6, offset));
+                }
+                else if (line.StartsWith("TERR"))
+                {
+                    sw.WriteLine(changePos(line, 3, offset));
+                }
+                else
+                    sw.WriteLine(line);
+            }
+            sw.Close();
+            MessageBox.Show("done");
+        }
+
+        private void btnCreateParam_Click(object sender, EventArgs e)
+        {
+            string[] p = pathR.Split('.');
+            pathW = p[0] + ".param";
+            sw = new StreamWriter(pathW, true);
+            string line;
+            string[] param = new string[778];
+            int counter = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.StartsWith("PARM"))
+                {
+                    string[] temp = line.Split(',');
+                    param[counter] = temp[2].Trim() + "," + temp[3].Trim();
+                    counter++;
+                }
+            }
+            Array.Sort(param);
+            for(int i = 0; i < param.Length; i++)
+            {
+                if(param[i] != null)
+                sw.WriteLine(param[i]);
+            }
+            sw.Close();
+            MessageBox.Show("done");
+        }
+
+        private void tbxOffset_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(tbxOffset.Text.Replace('.', ','), out offset);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sr.Close();
+            sw.Close();
+        }
+
+        private string changePos(string message, int pos, double offset)
+        {
+            string[] temp = message.Split(',');
+            string newLine = "";
+            double location = 0;
+
+            for (int i = 0; i <= temp.Length - 1; i++)
+            {
+                if (i == temp.Length - 1)
+                    newLine += temp[i];
+                else if (i == pos)
+                {
+                    double.TryParse(temp[i].Replace('.', ','), out location);
+                    if (location != 0)
+                        newLine += " " + (location + offset).ToString("F7", CultureInfo.InvariantCulture) + ", ";
+                    else
+                        newLine += temp[i] + ",";
+                }
+                else if (i == pos +1)
+                {
+                    double.TryParse(temp[i].Replace('.', ','), out location);
+                    if (location != 0)
+                        newLine += (location + offset).ToString("F7", CultureInfo.InvariantCulture) + ",";
+                    else
+                        newLine += temp[i] + ",";
+                }
+                else
+                    newLine += temp[i] + ",";
+            }
+            if(cbxLog.Checked && location != 0)
+            {
+                tbxLog.AppendText(message + "\n");
+                tbxLog.AppendText("=>" + newLine + "\n");
+            }
+            return newLine;
+        }
+
+    }
+}
